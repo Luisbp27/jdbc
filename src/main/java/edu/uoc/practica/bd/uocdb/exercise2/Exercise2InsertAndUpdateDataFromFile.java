be1d340b5bdcc44c3f5f5d539e8f850f4d8677e3
Luis Barca <luisbarcapons@gmail.com>
2023-05-29 19:44:02 +0200

    import deleted

--- src/main/java/edu/uoc/practica/bd/uocdb/exercise2/Exercise2InsertAndUpdateDataFromFile.java
+++ src/main/java/edu/uoc/practica/bd/uocdb/exercise2/Exercise2InsertAndUpdateDataFromFile.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 import java.text.ParseException;
